df3629359f7f380fa49058b05f3f307899b23a27
Tim Lord <tim@izettle.com>
2012-09-14 20:42:50 +0200

    Second (first?) hello world
    o Added appendum

---
+++ src/main/java/com/example/helloworld/core/Saying.java
@@ -0,0 +1,25 @@
+package com.example.helloworld.core;
+
+public class Saying {
+		private final long id;
+		private final String content;
+		private final String appendum;
+		
+		public Saying(long id, String content, String appendum) {
+				this.id = id;
+				this.content = content;
+				this.appendum = appendum;
+		}
+
+		public long getId() {
+				return id;
+		}
+
+		public String getContent() {
+				return content;
+		}
+
+		public String getAppendum() {
+				return appendum;
+		}
+}
